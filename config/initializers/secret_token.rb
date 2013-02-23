32d6a560f9b70808e754d6f12100ba64fa313db0
Carlos Solorio <csolorio05@gmail.com>
2013-02-23 14:36:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ardenreed2::Application.config.secret_token = 'ed074b1e5d0d5d3863132e996dd0ae793b20858dc6130d61a918f37a86aaac334bb91335ea365db95e7762c8c47f2401ac191914f5351cda64bddf8941b4225c'
