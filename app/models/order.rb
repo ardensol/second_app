c09db5d1d3da2df1a6f7c4c0123506c23857b480
Carlos Solorio <csolorio05@gmail.com>
2013-02-23 19:29:35 -0500

    added products and orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ActiveRecord::Base
-  attr_accessible :customer_id, :order_commission, :order_discount, :order_gross, :order_value
+  attr_accessible :customer_id, :order_commission, :order_discount, :order_gross, :order_value, :products_attributes
+  has_many :products
+  accepts_nested_attributes_for :products, allow_destroy: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
   attr_accessible :order_id, :product_name, :product_price
+  belongs_to :order
 end
